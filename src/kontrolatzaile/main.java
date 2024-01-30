4de06f50bc656015362ddc8e46917af0da2f90bb
in1dm3-d <in1dm3-d@elorrieta.int>
2024-01-30 11:46:14 +0100

    Cuarta prueba

--- src/kontrolatzaile/main.java
+++ src/kontrolatzaile/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es una prueba");
-		System.out.println("Paula como esto no te salga me cago en dios");
+		System.out.println("Iker como esto no te salga me cago en dios");
 	}
 
 }
