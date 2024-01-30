02b7dc8fa17a8700b4e13598b305c0057c953eb7
in1dm3-d <in1dm3-d@elorrieta.int>
2024-01-30 11:42:44 +0100

    Tercera prueba

--- src/kontrolatzaile/main.java
+++ src/kontrolatzaile/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Esto es una prueba");
+		System.out.println("Paula como esto no te salga me cago en dios");
 	}
 
 }
