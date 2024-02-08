ece117f9d6700864eacdd7d2456807f25500fc43
Mikel Martin <mikel.martinca@elorrieta-errekamari.com>
2024-02-08 12:27:35 +0100

    sprint1 aldatuta

--- src/modeloa/objetuak/Zinema.java
+++ src/modeloa/objetuak/Zinema.java
@@ -4,42 +4,45 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Zinema {
-	
+
 	private String izena;
 	private String helbidea;
 	private ArrayList<Aretoa> aretoList;
 	private ArrayList<Saioa> saioList;
+	private ArrayList<Karteldegia> pelikulalist;
 
-	//--------------------Konstruktorea--------------------//
+	// --------------------Konstruktorea--------------------//
 	public Zinema(String izena, String helbidea, ArrayList<Aretoa> aretoList, ArrayList<Saioa> saioList) {
 		this.izena = izena;
 		this.helbidea = helbidea;
 		this.aretoList = aretoList;
 		this.saioList = saioList;
 	}
-	//--------------------Konstruktorea--------------------//
-	
-	
-	//--------------------Set--------------------//
+	// --------------------Konstruktorea--------------------//
+
+	// --------------------Set--------------------//
 	public void setIzena(String izena) {
 		this.izena = izena;
 	}
-	
+
 	public void setHelbidea(String helbidea) {
 		this.helbidea = helbidea;
 	}
-	
+
 	public void setAretoList(ArrayList<Aretoa> aretoList) {
 		this.aretoList = aretoList;
 	}
-	
+
 	public void setSaioList(ArrayList<Saioa> saioList) {
 		this.saioList = saioList;
 	}
-	//--------------------Set--------------------//
-	
-	
-	//--------------------Get--------------------//
+
+	public void setFilma(ArrayList<Karteldegia> filma) {
+		this.pelikulalist = filma;
+	}
+	// --------------------Set--------------------//
+
+	// --------------------Get--------------------//
 	public String getIzena() {
 		return izena;
 	}
@@ -47,7 +50,7 @@ public class Zinema {
 	public String getHelbidea() {
 		return helbidea;
 	}
-	
+
 	public ArrayList<Aretoa> getAretoList() {
 		return aretoList;
 	}
@@ -55,24 +58,28 @@ public class Zinema {
 	public ArrayList<Saioa> getSaioList() {
 		return saioList;
 	}
-	//--------------------Get--------------------//
-	
-	
-	//--------------------toString--------------------//
+
+	public ArrayList<Karteldegia> getFilma() {
+		return pelikulalist;
+	}
+	// --------------------Get--------------------//
+
+	// --------------------toString--------------------//
+
 	@Override
 	public String toString() {
 		return "Zinema [izena=" + izena + ", helbidea=" + helbidea + ", aretoList=" + aretoList + ", saioList="
-				+ saioList + "]";
-	}
-	//--------------------toString--------------------//
-	
-	
-	//--------------------Equals--------------------//
-	@Override
-	public int hashCode() {
-		return Objects.hash(aretoList, helbidea, izena, saioList);
+				+ saioList + ", pelikulalist=" + pelikulalist + "]";
 	}
 
+	// --------------------toString--------------------//
+
+	// --------------------Equals--------------------//
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(aretoList, helbidea, izena, pelikulalist, saioList);
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -83,9 +90,11 @@ public class Zinema {
 		if (getClass() != obj.getClass())
 			return false;
 		Zinema other = (Zinema) obj;
-		return Objects.equals(izena, other.izena) && Objects.equals(helbidea, other.helbidea) && Objects.equals(aretoList, other.aretoList) 
+		return Objects.equals(aretoList, other.aretoList) && Objects.equals(helbidea, other.helbidea)
+				&& Objects.equals(izena, other.izena) && Objects.equals(pelikulalist, other.pelikulalist)
 				&& Objects.equals(saioList, other.saioList);
 	}
-	//--------------------Equals--------------------//
+
+	// --------------------Equals--------------------//
 
 }
