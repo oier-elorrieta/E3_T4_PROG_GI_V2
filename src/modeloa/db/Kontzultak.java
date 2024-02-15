97dfcc64d8de443be4ad45e47081aba091c5047f
Paula-Elorrieta <in1dm3-d@elorrieta.int>
2024-02-15 13:52:15 +0100

    Pelikula eta bezero dao eginda

--- src/modeloa/db/Kontzultak.java
+++ src/modeloa/db/Kontzultak.java
@@ -7,5 +7,5 @@ public interface Kontzultak {
 	
 	//Dao kontzultak
 	public static String bezeroa = "SELECT idBezero, NAN, izena, abizena, erabiltzailea, pasahitza, txartela, tlf_zbk, sexua FROM BEZEROA";
-	
+	public static String filma = "SELECT idFilma, izena, generoa, iraupena FROM FILMA";
 }
