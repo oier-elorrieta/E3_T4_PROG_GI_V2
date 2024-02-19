1587f2ca3350bce75ff30bd2b844a719b7a900c0
in1dm3-d <in1dm3-d@elorrieta.int>
2024-02-19 11:44:23 +0100

    DAO TESTAK SORTUTA BLA

--- src/test/AretoDAOTEST.java
+++ src/test/AretoDAOTEST.java
@@ -2,7 +2,16 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import org.junit.Test;
+
+import modeloa.db.Konexioa;
+import modeloa.db.Kontzultak;
+import modeloa.objetuak.Aretoa;
 
 public class AretoDAOTEST {
 
@@ -11,4 +20,31 @@ public class AretoDAOTEST {
 		fail("Not yet implemented");
 	}
 
+	 public ArrayList<Aretoa> lortuAreatoak() {
+	    	ArrayList<Aretoa> areatoak = new ArrayList<>();
+
+	        try {
+	            Konexioa.konexioa();
+
+	            PreparedStatement preparedStatement = Konexioa.konektatua.prepareStatement(Kontzultak.aretoa);
+	            ResultSet resultSet = preparedStatement.executeQuery();
+
+	            while (resultSet.next()) {
+	                String idAretoa = resultSet.getString("idAretoa");
+	                String izena = resultSet.getString("izena");
+
+	                Aretoa aretoa = new Aretoa(idAretoa, izena);
+	                areatoak.add(aretoa);
+	            }
+
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        } finally {
+	            Konexioa.konexioaExit();
+	        }
+
+	        return areatoak;
+	    }
+	
+	
 }
